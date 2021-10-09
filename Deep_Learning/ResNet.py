90f683d196942b7a2ad5a06f8c22be1ad65650bf
ShlokMohanty <59838234+ShlokMohanty@users.noreply.github.com>
2021-10-09 23:29:45 +0530

    Update ResNet.py

--- Deep_Learning/ResNet.py
+++ Deep_Learning/ResNet.py
@@ -1,22 +1,22 @@
 from keras.layers import Input, Lambda, Dense, Flatten 
 from keras.models import Model 
-from keras.applications.vgg16 import VGG16
-from keras.applications.vgg16 import preprocess_input
+from PIL import Image
+from keras.applications.resnet import ResNet50
 from keras.preprocessing.image import ImageDataGenerator
-from keras.preprocessing import image
 from keras.models import Sequential 
+import numpy as np
 from glob import glob
-
+import matplotlib.pyplot as plt
 IMAGE_SIZE = [224, 224]
 
 train_path = 'Datasets/Train'
 valid_path = 'Datasets/Test'
 
 # add preprocessing layer to the front of VGG
-vgg = VGG16(input_shape=IMAGE_SIZE + [3], weights='imagenet', include_top=False)
+resnet = ResNet50(input_shape=IMAGE_SIZE + [3], weights='imagenet', include_top=False)
 
 # don't train existing weights
-for layer in vgg.layers:
+for layer in resnet.layers:
   layer.trainable = False
   
 
@@ -26,12 +26,12 @@ folders = glob('Datasets/Train/*')
   
 
 # our layers - you can add more if you want
-x = Flatten()(vgg.output)
+x = Flatten()(resnet.output)
 # x = Dense(1000, activation='relu')(x)
 prediction = Dense(len(folders), activation='softmax')(x)
 
 # create a model object
-model = Model(inputs=vgg.input, outputs=prediction)
+model = Model(inputs=resnet.input, outputs=prediction)
 
 # view the structure of the model
 model.summary()
