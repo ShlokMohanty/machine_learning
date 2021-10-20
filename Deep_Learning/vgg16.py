224bfe57f8fefada8de6ff6e0d4472d50e0cd3ee
ShlokMohanty <59838234+ShlokMohanty@users.noreply.github.com>
2021-10-20 21:47:10 +0530

    Create vgg16.py

---
+++ Deep_Learning/vgg16.py
@@ -0,0 +1,93 @@
+from keras.layers import Input, Lambda, Dense, Flatten 
+from keras.models import Model 
+from PIL import Image
+from keras.applications.vgg16 import VGG16
+from keras.applications.vgg16 import preprocess_input 
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Sequential 
+import numpy as np
+from glob import glob
+import matplotlib.pyplot as plt
+IMAGE_SIZE = [224, 224]
+
+train_path = '/content/drive/MyDrive/Colab Notebooks/ Original Images/Datasets/ Training Set'
+valid_path = '/content/drive/MyDrive/Colab Notebooks/ Original Images/Datasets/ Testing Set'
+
+# add preprocessing layer to the front of VGG
+resnet = ResNet50(input_shape=IMAGE_SIZE + [3], weights='imagenet', include_top=False)
+
+# don't train existing weights
+for layer in resnet.layers:
+  layer.trainable = False
+  
+
+  
+  # useful for getting number of classes
+folders = glob('/content/drive/MyDrive/Colab Notebooks/ Original Images/Datasets/ Training Set')
+  
+
+# our layers - you can add more if you want
+x = Flatten()(resnet.output)
+# x = Dense(1000, activation='relu')(x)
+prediction = Dense(len(folders), activation='softmax')(x)
+
+# create a model object
+model = Model(inputs=resnet.input, outputs=prediction)
+
+# view the structure of the model
+model.summary()
+
+# tell the model what cost and optimization method to use
+model.compile(
+  loss='categorical_crossentropy',
+  optimizer='adam',
+  metrics=['accuracy']
+)
+
+
+from keras.preprocessing.image import ImageDataGenerator
+
+train_datagen = ImageDataGenerator(rescale = 1./255,
+                                   shear_range = 0.2,
+                                   zoom_range = 0.2,
+                                   horizontal_flip = True)
+
+test_datagen = ImageDataGenerator(rescale = 1./255)
+
+training_set = train_datagen.flow_from_directory('Datasets/Train',
+                                                 target_size = (224, 224),
+                                                 batch_size = 32,
+                                                 class_mode = 'categorical')
+
+test_set = test_datagen.flow_from_directory('Datasets/Test',
+                                            target_size = (224, 224),
+                                            batch_size = 32,
+                                            class_mode = 'categorical')
+
+'''r=model.fit_generator(training_set,
+                         samples_per_epoch = 8000,
+                         nb_epoch = 5,
+                         validation_data = test_set,
+                         nb_val_samples = 2000)'''
+
+# fit the model
+r = model.fit_generator(
+  training_set,
+  validation_data=test_set,
+  epochs=5,
+  steps_per_epoch=len(training_set),
+  validation_steps=len(test_set)
+)
+# loss
+plt.plot(r.history['loss'], label='train loss')
+plt.plot(r.history['val_loss'], label='val loss')
+plt.legend()
+plt.show()
+plt.savefig('LossVal_loss')
+
+# accuracies
+plt.plot(r.history['acc'], label='train acc')
+plt.plot(r.history['val_acc'], label='val acc')
+plt.legend()
+plt.show()
+plt.savefig('AccVal_acc')
