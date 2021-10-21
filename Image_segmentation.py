2bac7b44f93e5d488dcd4aa409e76054bc35177a
ShlokMohanty <59838234+ShlokMohanty@users.noreply.github.com>
2021-10-21 22:18:03 +0530

    Create Image_segmentation.py

---
+++ Image_segmentation.py
@@ -0,0 +1,19 @@
+from skimage.color import rgb2gray
+import numpy as np 
+import cv2
+import matplplotlib.pyplot as plt
+%matplotlib inline
+from scipy import ndimage
+image = plt.imread('Thor_Ragnarok.jpeg')
+image.shape
+plt.imshow(image)
+gray = rgb2gray(image)
+plt.imshow(gray, cmap='gray')
+gray.shape
+gray_r = gray.reshape(gray.reshape[0]*gray.shape[1])
+for i in range(gray_r.shape[0]):
+  if gray_r[i] > gray_r.mean():
+    gray_r[i]=1
+   else:
+    gray_r[i]=0
+  
