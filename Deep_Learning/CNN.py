6a321a397964abece0eaa00726e1dcced8d1edb8
Shlok Mohanty <59838234+ShlokMohanty@users.noreply.github.com>
2022-08-21 22:30:14 +0530

    created CNN.py

---
+++ Deep_Learning/CNN.py
@@ -0,0 +1,6 @@
+model = tf.keras.models.Sequential([
+  tf.keras.layers.Flatten(),
+  tf.keras.layers.Dense(128, activation=tf.nn.relu),
+  tf.keras.layers.Dense(10, activation=tf.nn.softmax)
+])
+#The input shape is not 
