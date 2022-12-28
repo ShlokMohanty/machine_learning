4808fb2d18afc6dd7ef73f785f6cfaed1208ac48
Shlok Mohanty <59838234+ShlokMohanty@users.noreply.github.com>
2022-12-28 19:32:27 +0530

    Create  test_parser.py

---
+++ aws_serverless_model/test/test_parser.py
@@ -0,0 +1,7 @@
+from unittest import TestCase 
+from unittest.mock import patch, Mock, call 
+from samtranslator.parser.parser import Parser 
+from samtranslator.plugins import LifeCycleEvents 
+from samtranslator.model.exceptions import InvalidDocumentationException, InvalidTemplateException,InvalidResourceException
+
+class TestParser(TestCase):
