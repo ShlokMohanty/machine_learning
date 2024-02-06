8e99cd10da0645f95addf069545fd610b4e4d6e3
Shlok Mohanty <59838234+ShlokMohanty@users.noreply.github.com>
2024-02-06 09:38:31 +0530

    Create perceptron_basic.py

---
+++ Deep_Learning/Hands_On_ML_Book /Basics/perceptron_basic.py
@@ -0,0 +1,11 @@
+import numpy as  np 
+from sklearn.datasets import load_iris
+from sklearn.linear_model import Perceptron 
+
+iris = load_iris()
+X = iris.data[:, (2,3)]
+y = (iris.target == 0).astype(np.int)
+
+per_clf = Perceptron()
+per_clf.fit(X,y)
+y_pred = per_clf.predict([[2, 0.5]])
